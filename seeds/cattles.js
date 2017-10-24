acecd7a54fb5057fc7c5efd7a5f5147e4d155bd2
Jose Julio Figueiredo Junior <josejuliojunior@hotmail.com>
2017-10-24 14:26:10 -0600

    more cattles seed

--- seeds/cattles.js
+++ seeds/cattles.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex.raw('DELETE FROM "cattles"; ALTER SEQUENCE cattles_id_seq RESTART WITH 5;')
+  return knex.raw('DELETE FROM "cattles"; ALTER SEQUENCE cattles_id_seq RESTART WITH 11;')
     .then(function () {
       const cattles = [{
         id: 1,
@@ -24,9 +24,45 @@ exports.seed = function(knex, Promise) {
       }, {
         id: 4,
         breedName: 'angus',
-        dob: '2016-06-16',
+        dob: '2015-07-16',
+        sex: 'male',
+        origin: 'Cebola'
+      },{
+        id: 5,
+        breedName: 'red angus',
+        dob: '2014-03-16',
+        sex: 'male',
+        origin: 'Diva'
+      },{
+        id: 6,
+        breedName: 'nelore',
+        dob: '2015-05-12',
         sex: 'male',
         origin: 'Alexandre'
+      },{
+        id: 7,
+        breedName: 'guzera',
+        dob: '2016-04-02',
+        sex: 'male',
+        origin: 'Rodrigo'
+      },{
+        id: 8,
+        breedName: 'brangus',
+        dob: '2016-01-19',
+        sex: 'male',
+        origin: 'Rodrigo'
+      },{
+        id: 9,
+        breedName: 'gir',
+        dob: '2016-09-29',
+        sex: 'male',
+        origin: 'Fernando'
+      },{
+        id: 10,
+        breedName: 'gir',
+        dob: '2015-11-28',
+        sex: 'male',
+        origin: 'Fernando'
       }];
 
       return knex('cattles').insert(cattles);
